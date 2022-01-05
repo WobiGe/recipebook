70b064338c61af836fe66ccb21e0e633f9f6f2f0
WobiGe <wladimir-g@gmx.de>
2022-01-05 16:02:49 +0100

    finished part 6 (databinding)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected =  new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe("Test", "Test Description", "http://cdn.shopify.com/s/files/1/0572/5909/8282/articles/BBQ-Burger-Rezept-Burger-vom-Grill_nwm1yp_82e595d6-69ed-4094-8a06-95956e21c113.jpg?v=1630322701"),
-    new Recipe("Test", "Test Description", "http://cdn.shopify.com/s/files/1/0572/5909/8282/articles/BBQ-Burger-Rezept-Burger-vom-Grill_nwm1yp_82e595d6-69ed-4094-8a06-95956e21c113.jpg?v=1630322701")
+    new Recipe("Test2", "Test2 Description", "http://cdn.shopify.com/s/files/1/0572/5909/8282/articles/BBQ-Burger-Rezept-Burger-vom-Grill_nwm1yp_82e595d6-69ed-4094-8a06-95956e21c113.jpg?v=1630322701")
 
   ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
