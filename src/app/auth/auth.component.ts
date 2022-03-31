fff68ef18a1be7be5d5ce7b7c7fa20bffcb904d9
WobiGe <wladimir-g@gmx.de>
2022-03-31 17:28:03 +0200

    add alertComponent dynamically

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,19 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, ComponentFactoryResolver, OnDestroy, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
+import { AlertComponent } from "../shared/alert/alert.component";
+import { PlaceHolderDirective } from "../shared/placeholder/placeholder.directive";
 import { AuthResponseData, AuthService } from "./auth.service";
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
-export class AuthComponent{
+export class AuthComponent implements OnDestroy{
   userLoggedIn = true;
   isLoading = false;
   error: string = null;
+  private closeSub: Subscription;
+  @ViewChild(PlaceHolderDirective, {static: false}) alertHost: PlaceHolderDirective;
 
-  constructor(private authService: AuthService, private router: Router){}
+  constructor(private authService: AuthService,
+              private router: Router,
+              private cfr: ComponentFactoryResolver){}
 
   onSwitchMode(){
     this.userLoggedIn = !this.userLoggedIn;
@@ -45,6 +51,7 @@ export class AuthComponent{
       error: (errorMessage) => {
         console.log(errorMessage);
         this.error = errorMessage;
+        this.showErrorAlert(errorMessage);
         this.isLoading = false;
       }
     });
@@ -52,7 +59,28 @@ export class AuthComponent{
     form.reset();
   }
 
+  private showErrorAlert(error: string){
+    const alertComponentFactory = this.cfr.resolveComponentFactory(AlertComponent);
+    const hostVcr = this.alertHost.vcr;
+    hostVcr.clear();
+
+    const compRef = hostVcr.createComponent(alertComponentFactory);
+
+    compRef.instance.message = error;
+    this.closeSub = compRef.instance.close.subscribe(()=>{
+      this.closeSub.unsubscribe();
+      hostVcr.clear();
+    });
+
+  }
+
   onHandleError(){
     this.error = null;
+  }
+
+  ngOnDestroy(): void {
+      if (this.closeSub){
+        this.closeSub.unsubscribe();
+      }
   }
 }
